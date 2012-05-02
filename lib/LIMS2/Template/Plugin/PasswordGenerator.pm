1d5b9897ba36b591ea2015bd9d3e5f4492bf0231
Ray Miller <rm7@sanger.ac.uk>
2012-05-02 14:54:50 +0100

    PerlTidy

--- lib/LIMS2/Template/Plugin/PasswordGenerator.pm
+++ lib/LIMS2/Template/Plugin/PasswordGenerator.pm
@@ -5,17 +5,17 @@ use warnings FATAL => 'all';
 
 use base qw( Template::Plugin Class::Data::Inheritable );
 
-__PACKAGE__->mk_classdata( PW_CHARS => [ "A".."Z", "a".."z", "0".."9" ] );
+__PACKAGE__->mk_classdata( PW_CHARS => [ "A" .. "Z", "a" .. "z", "0" .. "9" ] );
 __PACKAGE__->mk_classdata( PW_LEN => 12 );
 
 sub generate_password {
     my ( $class, $pw_len ) = @_;
 
-    $pw_len ||= $class->PW_LEN;    
+    $pw_len ||= $class->PW_LEN;
 
     my $pw_chars = $class->PW_CHARS;
-    
-    join '', map { $pw_chars->[ int rand @{$pw_chars} ] } 1..$pw_len;
+
+    join '', map { $pw_chars->[ int rand @{$pw_chars} ] } 1 .. $pw_len;
 }
 
 1;
