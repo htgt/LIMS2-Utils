57a2466bcb0ecb93055cc7146c4d4aa3ba23057d
Alex Hodgkins <ah19@sanger.ac.uk>
2013-07-04 10:37:10 +0100

    Added quotes around the done criteria as sometimes it will produce a bash syntax error.

--- lib/LIMS2/Util/FarmJobRunner.pm
+++ lib/LIMS2/Util/FarmJobRunner.pm
@@ -126,7 +126,7 @@ sub _build_job_dependency {
     return () unless @{ $dependencies };
 
     #this creates a list of dependencies, for example 'done(12) && done(13) && done(14)'
-    return ( '-w', join( " && ", map { 'done(' . $_ . ')' } @{ $dependencies } ) );
+    return ( '-w', '"' . join( " && ", map { 'done(' . $_ . ')' } @{ $dependencies } ) . '"' );
 }
 
 sub _run_cmd {
@@ -197,6 +197,11 @@ Helper module for running bsub jobs from LIMS2/The VMs.
 Sets the appropriate environment for using our perlbrew install in /software.
 
 The default queue is normal, and the default memory required is 2000 MB.
+
+=head1 TODO
+
+Write a jobarray wrapper that will take a yaml file or something, so that we can support params,
+and wrap anything into a jobarray.
 
 =head1 AUTHOR
 
